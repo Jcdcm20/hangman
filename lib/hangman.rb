c68a99d67fba4e1c41a8742e68d8fa86b213c135
jcdcm <jcdcm20@gmail.com>
2023-09-25 15:10:36 -0300

    Start using classes

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,16 +1,22 @@
-def pick_word
+class Hangman
+  attr_accessor :word
+  def initialize
+    @word = pick_word
+  end
 
-  File.open('google-10000-english-no-swears.txt', 'r') do |file|
-    dict = []
-    file.each do |line|
-      line.chomp!
-      if line.length >= 5 && line.length <= 12
-        dict.push(line)
+  def pick_word
+    File.open('google-10000-english-no-swears.txt', 'r') do |file|
+      dict = []
+      file.each do |line|
+        line.chomp!
+        if line.length >= 5 && line.length <= 12
+          dict.push(line)
+        end
       end
+      dict[rand(dict.length)]
     end
-    r = rand(dict.length)
-
-    return dict[r]
   end
 end
-word = pick_word
+
+game = Hangman.new
+puts game.word
